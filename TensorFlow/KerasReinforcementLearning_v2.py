69c1fef6aa155f71a5d8711e4a585039f783870d
gilyaary <gilyaary@gmail.com>
2018-11-04 14:05:39 -0800

    adding deep q learning

--- TensorFlow/KerasReinforcementLearning_v2.py
+++ TensorFlow/KerasReinforcementLearning_v2.py
@@ -87,6 +87,7 @@ class DQNAgent:
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
+        #self.learning_rate = 0.001
         self.learning_rate = 0.01
         self.model = self._build_model()
     def _build_model(self):
@@ -110,7 +111,7 @@ class DQNAgent:
 
 agent =  DQNAgent(9,9)
 game = Game()
-number_of_episodes = 10000
+number_of_episodes = 100000
 total_rewards = 0
 for episode_count in range(number_of_episodes):
     game.reset()
